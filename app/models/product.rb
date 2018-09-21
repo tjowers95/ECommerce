412aff628e5c06239a8d34152526a55048bf6c09
Tyler Jowers <Jowers.tc@protonmail.com>
2018-09-21 19:45:48 -0400

    Added orders

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
  validates_numericality_of :price
-
  validates :stock, numericality: {only_integer: true, greater_than_or_equal_to: 0} 
-
+ has_many :order_items
 end
